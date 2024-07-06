b7191c381556fb91831e6781e125585a42a6568f
FelipeAugst <felipeaug92@gmail.com>
2024-07-06 18:58:19 -0300

    start

---
+++ myRange.py
@@ -0,0 +1,50 @@
+import sys
+
+class MyRange:
+    step=1
+    stop=0
+    start=0
+   
+    def __init__(self,stop,start=0,step=1):
+        self.start=start
+        self.stop=stop
+        self.step=step
+        if self.step !=1 or self.start !=0:
+            self.start,self.stop = self.stop,self.start
+           
+   
+   
+    def __iter__(self):
+        if not self.valid():
+            raise BaseException("Intervalo invalido!")
+            return
+        return self.__next__()
+   
+    def __next__(self):
+        s = self.start
+        if self.start <= self.stop:
+            while s < self.stop:
+                yield s
+                s += self.step
+        else:
+            while s > self.stop:
+                yield s
+                s += self.step
+   
+   
+   
+           
+    def valid(self):
+        if (self.step==0) or (self.start > self.stop and self.step >= 0) or (self.start < self.stop and self.step <=0):
+            return False
+        else:
+            return True
+   
+   
+   
+   
+   
+
+for n in sys.argv:
+    print(n)
+
